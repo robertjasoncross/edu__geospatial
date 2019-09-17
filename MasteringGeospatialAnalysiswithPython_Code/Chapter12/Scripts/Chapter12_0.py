6d205e4d80a00974ac734a375551a4d39b845261
robertjasoncross <jason@thepeculiarform.com>
2019-09-17 17:04:32 -0600

    updates

--- MasteringGeospatialAnalysiswithPython_Code/Chapter12/Scripts/Chapter12_0.py
+++ MasteringGeospatialAnalysiswithPython_Code/Chapter12/Scripts/Chapter12_0.py
@@ -7,7 +7,7 @@ cursor.execute('CREATE DATABASE chapter122')
 connection.close() 
 
 
-connection = psycopg2.connect(dbname='chapter12', host='localhost',
+connection = psycopg2.connect(dbname='ch12', host='localhost',
  user='{user}', password='{password}', port="5432")
 cursor = connection.cursor()
 connection.autocommit = True
